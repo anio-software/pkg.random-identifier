708996082a5b80f1812b059ea94b1bcef73bd7a4
eXory2024 <exory@anio.software>
2024-12-09 15:06:45 +0100

    Add examples code.

---
+++ examples/example.mjs
@@ -0,0 +1,7 @@
+import {
+	randomIdentifierSync
+} from "../dist/default/index.min.mjs"
+
+const identifier = randomIdentifierSync(16) // create a random identifier of length 16
+
+console.log(identifier) // 84b6af1d1572de65
